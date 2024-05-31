4bf7bf9dd641430a14c1fbee47dd4c139b0535c7
David Brown <dbrown92700@gmail.com>
2024-05-31 17:10:23 -0400

    Added SLAC capability

--- slac_report.py
+++ slac_report.py
@@ -9,7 +9,7 @@ class SlacReport:
         self.upload_payload = {}
         self.poll_payload = {}
         self.sys_info = {}
-        self.auth_reqeust = {}
+        self.auth_request = {}
 
     def read_file(self, filename='slac.txt'):
 
@@ -38,7 +38,7 @@ class SlacReport:
     def create_slac_req_payload(self, virtual_account=''):
 
         self.sys_info = json.loads(self.slac_req_json['smartLicense']['smartLicenseSystemInfo']['data'])
-        self.auth_reqeust = json.loads(self.slac_req_json['smartLicense']['smartLicenseAuthRequest']['data'])
+        self.auth_request = json.loads(self.slac_req_json['smartLicense']['smartLicenseAuthRequest']['data'])
 
         self.upload_payload = {
             "data": {
@@ -48,13 +48,22 @@ class SlacReport:
                 "device_type": "DNA On Prem",
                 "licenses": [
                     {
-                        "sudi": self.auth_reqeust[0]['sudi'],
+                        "sudi": {
+                            "udi_pid": self.sys_info['sudi']['udi_pid'],
+                            "udi_serial_number": self.sys_info['sudi']['udi_serial_number'],
+                            "host_identifier": self.sys_info['hostname'],
+                            "mac_address": "NA",
+                            "udi_vid": "NA",
+                            "uuid": f"{self.sys_info['sudi']['udi_pid']}/{self.sys_info['sudi']['udi_serial_number']}",
+                            "suvi": "NA",
+                            "tenant_id": "NA"
+                        },
                         "software_tag_identifier": self.sys_info['device_list'][0]['software_tag_identifier'],
                         "hostname": self.sys_info['device_list'][0]['hostname'],
                         "trust_request": self.sys_info['device_list'][0]['hostname'],
-                        "keys": self.auth_reqeust[0]['keys'],
-                        "confirm_code": self.auth_reqeust[0]['confirm_code'],
-                        "remove_code": self.auth_reqeust[0]['remove_code']
+                        "keys": self.auth_request[0]['keys'],
+                        "confirm_code": self.auth_request[0]['confirm_code'],
+                        "remove_code": self.auth_request[0]['remove_code']
                     }
                 ]
             }
